3a70852f2dfc1b4f460960aaae90515b19462915
owen <hust2014owen@gmail.com>
2019-12-24 20:00:02 -0500

    调色板补充，以及mock相关

---
+++ 1-learn-python/mymodule.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+def rm(filename):
+    os.remove(filename)
---
+++ 1-learn-python/simple.py
@@ -0,0 +1,6 @@
+def simple_function():
+    return "You have called simple_function"
+
+class SimpleClass(object):
+    def explode(self):
+        return "KABOOM!"
